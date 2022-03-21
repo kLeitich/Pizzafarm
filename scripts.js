74b961f2b96c0832c3533bff87aea5bed4bf3ea7
kLeitich <kevleitich@gmail.com>
2022-03-21 16:08:12 +0300

    adding first trial for js

---
+++ scripts.js
@@ -0,0 +1,34 @@
+// $('.Delivery').hide();
+// $('.pizza1,.Delivery').on(function(){
+// 		$('.pizza1,.Delivery').toggle();
+// });
+
+
+// $('.Delivery').hide();
+// $('.btn').on('click',
+//   function() {
+//     $('.pizza1, .Delivery').toggle(200);
+//   }
+// );
+
+// var delivery =function(option,onclick){
+//     var option=document.querySelector('input[name="option"]:checked').value;
+//     console.log(option)
+//     if (option=="Delivery"){
+//         onclick('.form-check-input').$('.deliveryform').show();
+//     }else{
+//         onclick('.form-check-input').$('.deliveryform').hide();
+//     }
+
+// };
+// delivery(option,'onclick')
+
+// let selected = document.getElementsByClassName('form-select').value(option:selected);
+// console.log(selected)
+
+// $(document).ready(function(){
+//     $("select.form-select1").change(function(){
+//         var selected = $(this).children("option:selected").val();
+//         alert("You have selected the country - " + selectedCountry);
+//     });
+// });
