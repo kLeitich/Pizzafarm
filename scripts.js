7c636246756e4bd426a623a1f3f4170cf6ae7b7f
kLeitich <kevleitich@gmail.com>
2022-03-22 18:12:48 +0300

    adding final fix

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 class Pizza{
-    constructor(size,crust,toppings,noofpizzas,delivery){
+    constructor(size,crust,toppings,noofpizzas,option){
         this.size =size;
         this.crust = crust;
         this.toppings= toppings;
         this.noofpizzas=noofpizzas;
-        this.delivery=delivery;
+        this.option= option;
        
     }
     
 
     totalAmount(){
-        const total=((parseInt(this.size)+parseInt(this.crust)+parseInt(this.toppings))*parseInt(this.noofpizzas))+parseInt(this.delivery);
+        const total=((parseInt(this.size)+parseInt(this.crust)+parseInt(this.toppings))*parseInt(this.noofpizzas))+parseInt(this.option);
         return total
     }
 };
@@ -22,34 +22,37 @@ class Pizza{
 
 let form = document.getElementById("userInput");
 let form1 = document.getElementById("deliveryForm");
-let form2 = document.getElementById("deliveryOption");
+let option = document.getElementById("option");
 let size = document.getElementById("size");
 let crust = document.getElementById("crust");
 let toppings = document.getElementById("toppings");
 let noOfPizza = document.getElementById("noOfPizza");
-let delivery = document.getElementById("inputState");
 let output = document.querySelector(".output");
 let summary = document.querySelector("#summary");
 let summary1 = document.querySelector("#summary1");
 let summary2 = document.querySelector("#summary2");
+let summary3 = document.querySelector("#summary3");
+
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
-    let userOrder = new Pizza(size.value,crust.value,toppings.value,noOfPizza.value,delivery.value)
-    
+    let userOrder = new Pizza(size.value,crust.value,toppings.value,noOfPizza.value,option.value)
 
 
     summary1.append("your size price; "+userOrder.size+ " Ksh");
     summary2.append("your crust price; "+userOrder.crust+ " Ksh");
     summary.append('Your toppings price: '+userOrder.toppings+' Ksh');
+    summary3.append('Your delivery fee is: '+userOrder.option+' Ksh');
     
   
   });
   form1.addEventListener('submit',(event)=>{
     event.preventDefault();
-    let userOrder = new Pizza(size.value,crust.value,toppings.value,noOfPizza.value,delivery.value)
+    let userOrder = new Pizza(size.value,crust.value,toppings.value,noOfPizza.value,option.value);
     const amount = userOrder.totalAmount();
     output.append( 'Your TOTAL IS:'+ amount+ " KSh");
-    alert("Your order has been recieved")
+    alert("Your order has been recieved,Thank you for ordering with us.");
+    
   
   });
+
