ad49afa405aea01da451506334a0467e5552c22b
kLeitich <kevleitich@gmail.com>
2022-03-22 15:20:42 +0300

    adding business logic to the js file

--- scripts.js
+++ scripts.js
@@ -1,34 +1,66 @@
-// $('.Delivery').hide();
-// $('.pizza1,.Delivery').on(function(){
-// 		$('.pizza1,.Delivery').toggle();
-// });
+class Pizza{
+    constructor(size,crust,toppings,noofpizzas,delivery){
+        this.size =size;
+        this.crust = crust;
+        this.toppings= toppings;
+        this.noofpizzas=noofpizzas;
+        this.delivery=delivery;
+       
+    }
+    
+
+    totalAmount(){
+        const total=((parseInt(this.size)+parseInt(this.crust)+parseInt(this.toppings))*parseInt(this.noofpizzas))+parseInt(this.delivery);
+        return total
+    }
+};
 
 
-// $('.Delivery').hide();
-// $('.btn').on('click',
-//   function() {
-//     $('.pizza1, .Delivery').toggle(200);
-//   }
-// );
 
-// var delivery =function(option,onclick){
-//     var option=document.querySelector('input[name="option"]:checked').value;
-//     console.log(option)
-//     if (option=="Delivery"){
-//         onclick('.form-check-input').$('.deliveryform').show();
-//     }else{
-//         onclick('.form-check-input').$('.deliveryform').hide();
-//     }
 
-// };
-// delivery(option,'onclick')
 
-// let selected = document.getElementsByClassName('form-select').value(option:selected);
-// console.log(selected)
 
-// $(document).ready(function(){
-//     $("select.form-select1").change(function(){
-//         var selected = $(this).children("option:selected").val();
-//         alert("You have selected the country - " + selectedCountry);
-//     });
-// });
+let form = document.getElementById("userInput");
+let form1 = document.getElementById("deliveryForm");
+let form2 = document.getElementById("deliveryOption");
+let size = document.getElementById("size");
+let crust = document.getElementById("crust");
+let toppings = document.getElementById("toppings");
+let noOfPizza = document.getElementById("noOfPizza");
+let delivery = document.getElementById("inputState");
+let output = document.querySelector(".output");
+let summary = document.querySelector("#summary");
+let summary1 = document.querySelector("#summary1");
+let summary2 = document.querySelector("#summary2");
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let userOrder = new Pizza(size.value,crust.value,toppings.value,noOfPizza.value,delivery.value)
+    
+
+
+    summary1.append("your size price; "+userOrder.size+ " Ksh");
+    summary2.append("your crust price; "+userOrder.crust+ " Ksh");
+    summary.append('Your toppings price: '+userOrder.toppings+' Ksh');
+    
+  
+  });
+  form1.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let userOrder = new Pizza(size.value,crust.value,toppings.value,noOfPizza.value,delivery.value)
+    const amount = userOrder.totalAmount();
+
+    output.append( 'Your TOTAL IS:'+ amount+ " KSh");
+  
+  });
+//   form2.addEventListener('click',()=>{
+//     // event.preventDefault();
+//     document.getElementById("flexRadioDefault1").click(function(){
+//         $(".form-group").hide();
+//       });
+      
+//       document.getElementById("flexRadioDefault2").click(function(){
+//         $(".form-group").show();
+//       });
+  
+//   });
